64a005972c646222722fe7331d6e180a518ccafc
Antonio Ortiz <ortiz.antonio@gmail.com>
2026-03-25 15:06:11 -0500

    mobile version

--- src/design-system/ui.ts
+++ src/design-system/ui.ts
@@ -2,9 +2,6 @@ const UI_TEXT = `
   break-words 
   text-base font-base font-regular line-height-standard
 `
-
-const UI_TEXT_TITLE = `font-bold`
-
 export const UI_BTN_ACCENT = `
   bg-accent
 `
@@ -23,28 +20,8 @@ export default {
     ${UI_TEXT}
   `,
 
-  'ui-text-sm': `
-    text-step-0
-    ${UI_TEXT}
-  `,
-
   'ui-text-title-1': `
-    text-step-4
-    ${UI_TEXT_TITLE}
-  `,
-
-  'ui-text-title-2': `
-    text-step-3
-    ${UI_TEXT_TITLE}
-  `,
-
-  'ui-text-title-3': `
     text-step-2
-    ${UI_TEXT_TITLE}
-  `,
-
-  'ui-text-title-4': `
-    text-step-1
-    ${UI_TEXT_TITLE}
+    font-bold 
   `,
 }
